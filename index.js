ee54125d742a969f20d0bcabcce5cf6a90965810
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-05 15:47:21 -0400

    phase-1-object-orientation-in-javascript
    phase-1-object-orientation-in-javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+function sayHello(nameOfPerson) {
+    console.log(`Hello, my name is ${nameOfPerson}.`);
+}
+
+function sayAge(age) {
+    console.log(`I am ${age} years old.`);
+}
+
+function haveBirthday(age) {
+    return age + 1;
+}
+sayHello(name);
+
+
+haveBirthday() {
+    console.log("It's my birthday!");
+    this.age += 1;
+}
